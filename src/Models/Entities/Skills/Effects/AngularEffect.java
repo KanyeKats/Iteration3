ff812b7b822f8562cbd47622c6906d3f3102d79b
Josh <joshnassar94@gmail.com>
2016-04-06 13:18:56 -0400

    Added skeleton classes for InfluenceEffects, finishing off the skeleton for skills

---
+++ src/Models/Entities/Skills/Effects/AngularEffect.java
@@ -0,0 +1,15 @@
+package Models.Entities.Skills.Effects;
+
+/**
+ * Created by josh on 4/6/16.
+ */
+public class AngularEffect extends Effect {
+
+    protected void traverseThroughTiles() {
+
+    }
+
+    protected void apply() {
+
+    }
+}
