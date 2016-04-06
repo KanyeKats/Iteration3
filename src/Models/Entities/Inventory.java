870b204bb60b4e10b62e90baa513a7f2908ed262
dnkllhr <dnkllhr@gmail.com>
2016-04-06 19:11:30 -0400

    made skeleton classes, and still working on entity and occupation.

---
+++ src/Models/Entities/Inventory.java
@@ -0,0 +1,49 @@
+package Models.Entities;
+
+import Models.Items.Item;
+
+/**
+ * Created by Aidan on 4/6/2016.
+ */
+public class Inventory {
+
+    private Item[] items; //this can be an arraylist as well
+    private int numOfItems;
+    private int inventorySize;
+
+    public Inventory(int inventorySize){
+        this.items = new Item[inventorySize];
+        for(Item item: items){
+            item = null;
+        }
+        this.inventorySize = inventorySize;
+        numOfItems = 0;
+    }
+
+    public void addItem(Item item){
+        if(isFull()){
+            return;
+        }
+
+        for(Item inventoryItem: items)
+            if(inventoryItem == null){
+                inventoryItem = item;
+            }
+        numOfItems++;
+    }
+
+    public boolean isFull(){
+        return(numOfItems == inventorySize);
+    }
+
+    //The contains(ID) class Rokas wanted
+    public boolean containsID(int ID){
+        for(Item item: items){
+            if(item.getID() == ID){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- src/Models/Items/Item.java
+++ src/Models/Items/Item.java
@@ -4,4 +4,18 @@ package Models.Items;
  * Created by Bradley on 4/5/2016.
  */
 public class Item {
+    //just needed to use item for its ID
+    public int ID;
+
+
+
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
