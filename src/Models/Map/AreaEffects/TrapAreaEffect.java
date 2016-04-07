1f2b63c825f1fc6a2a5eff52418d22bc5289df2b
kaufmann42 <kaufmann43@gmail.com>
2016-04-07 01:54:38 -0400

    fixed the trap areaeffect

--- src/Models/Map/AreaEffects/TrapAreaEffect.java
+++ src/Models/Map/AreaEffects/TrapAreaEffect.java
@@ -1,14 +1,30 @@
 package Models.Map.AreaEffects;
 
-import Models.Entities.Skills.Consequences.PeriodicStatConsequence;
+import Models.Entities.Skills.Consequences.ImmediateStatConsequence;
 import Models.Map.AreaEffect;
 import Models.Map.Decal;
+
+import java.util.Random;
 
 /**
  * Created by johnkaufmann on 4/7/16.
  */
 public class TrapAreaEffect extends AreaEffect {
-    public TrapAreaEffect(int damage, int speedReduction) {
-        super(new PeriodicStatConsequence().makeTrap(damage, speedReduction), Decal.trap);
+    private boolean isVisible;
+    public TrapAreaEffect(int damage) {
+        super(new ImmediateStatConsequence().makeDamage(damage), Decal.trap);
+    }
+
+    public void attemptToDetectTrap(int chanceToDetect) {
+        Random rand = new Random();
+
+        int  n = rand.nextInt(100);
+
+        if (chanceToDetect < n) return;
+        else this.isVisible = true;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
     }
 }
