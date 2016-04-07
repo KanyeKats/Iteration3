621cda912b3d8bb769c0e517d10a52e922d46983
Joshua Nassar <joshnassar94@gmail.com>
2016-04-07 15:11:54 -0400

    PassiveSkills completely done (I think)

--- src/Models/Entities/Skills/PassiveSkills/SmasherSkills/TwoHandedWeaponMastery.java
+++ src/Models/Entities/Skills/PassiveSkills/SmasherSkills/TwoHandedWeaponMastery.java
@@ -2,22 +2,35 @@ package Models.Entities.Skills.PassiveSkills.SmasherSkills;
 
 import Models.Entities.Skills.PassiveSkills.PassiveSkill;
 import Models.Entities.Skills.PassiveSkills.PassiveSkillsVisitors.PassiveSkillVisitor;
+import Models.Entities.Stats.Stat;
 import Models.Entities.Stats.StatModification;
+import Models.Entities.Stats.Stats;
 
 /**
  * Created by josh on 4/6/16.
  */
 public class TwoHandedWeaponMastery extends PassiveSkill{
 
-    public void addToStats(PassiveSkillVisitor passiveSkillVisitor){
-        passiveSkillVisitor.activate(this);
+    public TwoHandedWeaponMastery(Stats stats){
+        this.stats = stats;
+        level = 0;
+        statModification = new StatModification(Stat.WEAPON_MODIFIER, level);
+        addToStats();
+    }
+
+    @Override
+    public void incrementLevel(){
+        ++level;
+        removeFromStats();
+        statModification = new StatModification(Stat.WEAPON_MODIFIER, level);
+        addToStats();
     }
 
     // Blank overriden interface method added here so that the codebase compiles.
     // TODO: implement function
     @Override
     public void acceptVisitor(PassiveSkillVisitor passiveSkillVisitor) {
-
+        passiveSkillVisitor.activate(this);
     }
 
 
--- src/Models/Entities/Skills/Skill.java
+++ src/Models/Entities/Skills/Skill.java
@@ -1,11 +1,10 @@
 package Models.Entities.Skills;
 
-
 /**
  * Created by josh on 4/6/16.
  */
 public abstract class Skill {
-    private int level;
+    protected int level;
 
     public void incrementLevel(){
         ++level;
