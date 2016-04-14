b3593a904cc620d63cda72a992d8e385c61be011
Rokas <rokaslopas@gmail.com>
2016-04-14 18:40:13 -0400

    updated with master

---
+++ src/Utilities/Toast.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Magic_Buddha on 4/13/2016.
+ */
+public class Toast {
+    //holds toasts queue
+    private static LinkedList<String> messages = new LinkedList<>();
+
+    public static void addMessage(String message, int duration) {
+
+    }
+}
