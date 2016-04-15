1cd188e7f785120bd18b7f8ccd12b48bf8608074
Sergio Puleri <spuleri@ufl.edu>
2016-04-15 18:31:04 -0400

    Add fow direction decals for rivers

--- src/Models/Map/AreaEffects/RiverAreaEffect.java
+++ src/Models/Map/AreaEffects/RiverAreaEffect.java
@@ -8,8 +8,11 @@ import Models.Entities.Stats.Stats;
 import Models.Map.AreaEffect;
 import Models.Map.Decal;
 import Models.Map.Direction;
+import Views.Graphics.Assets;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
 
 /**
  * Created by johnkaufmann on 4/7/16.
@@ -20,17 +23,33 @@ public class RiverAreaEffect extends AreaEffect {
     private Direction flowDirection;
     private int flowSpeed;
 
+    private HashMap<Direction, BufferedImage> flowImages;
+
     public RiverAreaEffect(Direction flowDirection, int flowSpeed) {
         // Set instance variables
         this.flowDirection = flowDirection;
         this.flowSpeed = flowSpeed;
 
+        // Create hashmap of direction to image
+        initFlowImages();
+
         // Set decal
-        this.decal = Decal.river();
+        this.decal = new Decal(flowImages.get(this.flowDirection));
+//        this.decal = Decal.river();
 
         // Set visibility and removability
         this.isVisible = true;
         this.isRemovable = false;
+    }
+
+    private void initFlowImages(){
+        this.flowImages = new HashMap<>();
+        flowImages.put(Direction.NORTH, Assets.FLOW_N);
+        flowImages.put(Direction.NORTH_EAST, Assets.FLOW_NE);
+        flowImages.put(Direction.SOUTH_EAST, Assets.FLOW_SE);
+        flowImages.put(Direction.SOUTH, Assets.FLOW_S);
+        flowImages.put(Direction.SOUTH_WEST, Assets.FLOW_SW);
+        flowImages.put(Direction.NORTH_WEST, Assets.FLOW_NW);
     }
 
     @Override
