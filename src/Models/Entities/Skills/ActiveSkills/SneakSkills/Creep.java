2dd691fc4da36e44b8914c02ef36b482abbd5d54
kaufmann42 <kaufmann43@gmail.com>
2016-04-17 20:16:44 -0400

    made it so you get faster not slower lol

--- src/Models/Entities/Skills/ActiveSkills/SneakSkills/Creep.java
+++ src/Models/Entities/Skills/ActiveSkills/SneakSkills/Creep.java
@@ -17,7 +17,7 @@ public class Creep extends ActiveSkill {
 
     public final int BASE_COOLDOWN_TIME = 20000;    //20 seconds
     public final int BASE_EFFECT_TIME = 10000;      //10 seconds
-    private final int BASE_MOVEMENT_DECREMENT = -2;
+    private final int BASE_MOVEMENT_DECREMENT = -6;
     private final int BASE_OFFENSIVE_INCREMENT = 3;
     private Entity entity;
 
@@ -49,7 +49,7 @@ public class Creep extends ActiveSkill {
     public void incrementLevel() {
         ++level;
         StatModificationList statModificationList = new StatModificationList(
-                new StatModification(Stat.MOVEMENT,BASE_MOVEMENT_DECREMENT*level),
+                new StatModification(Stat.MOVEMENT,BASE_MOVEMENT_DECREMENT-level),
                 new StatModification(Stat.OFFSENSIVE_RATING,BASE_OFFENSIVE_INCREMENT*level));
         this.consequence = new TimedConsequence(statModificationList, BASE_EFFECT_TIME);
     }
